a5bac2b251e6b880e00a300e995dbea0d91b6de6
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-08-24 17:29:23 -0500

    Images and metadata

--- userlist/pages/about.js
+++ userlist/pages/about.js
@@ -1,10 +1,18 @@
+import Head from "next/head";
+
 const About = () => {
     return ( 
-        <div>
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem et, recusandae fuga laboriosam ipsam ipsum nihil sit enim totam, nulla molestiae. Ut hic obcaecati possimus recusandae nobis voluptas cumque quos.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem et, recusandae fuga laboriosam ipsam ipsum nihil sit enim totam, nulla molestiae. Ut hic obcaecati possimus recusandae nobis voluptas cumque quos.</p>
-        </div>
+        <>
+            <Head>
+                <title>User List | About</title>
+                <meta name="keywords" content="users" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem et, recusandae fuga laboriosam ipsam ipsum nihil sit enim totam, nulla molestiae. Ut hic obcaecati possimus recusandae nobis voluptas cumque quos.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem et, recusandae fuga laboriosam ipsam ipsum nihil sit enim totam, nulla molestiae. Ut hic obcaecati possimus recusandae nobis voluptas cumque quos.</p>
+            </div>
+        </>
     );
 }
  
--- userlist/pages/index.js
+++ userlist/pages/index.js
@@ -1,15 +1,22 @@
 import Link from "next/link"
 import styles from '../styles/Home.module.css'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem et, recusandae fuga laboriosam ipsam ipsum nihil sit enim totam, nulla molestiae. Ut hic obcaecati possimus recusandae nobis voluptas cumque quos.</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem et, recusandae fuga laboriosam ipsam ipsum nihil sit enim totam, nulla molestiae. Ut hic obcaecati possimus recusandae nobis voluptas cumque quos.</p>
-      <Link href="/users">
-        <a className={styles.btn}>See User Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>User List | Home</title>
+        <meta name="keywords" content="users" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem et, recusandae fuga laboriosam ipsam ipsum nihil sit enim totam, nulla molestiae. Ut hic obcaecati possimus recusandae nobis voluptas cumque quos.</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem et, recusandae fuga laboriosam ipsam ipsum nihil sit enim totam, nulla molestiae. Ut hic obcaecati possimus recusandae nobis voluptas cumque quos.</p>
+        <Link href="/users">
+          <a className={styles.btn}>See User Listing</a>
+        </Link>
+      </div>
+    </>
   )
 }
